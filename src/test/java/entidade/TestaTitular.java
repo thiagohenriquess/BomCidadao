00a301e9a9c7e77d26522ca0d0210113e318a928
Thiago Santos <thiago.santos@m4u.com.br>
2016-12-05 15:49:09 -0200

    Aprimoradno testes

--- src/test/java/entidade/TestaTitular.java
+++ src/test/java/entidade/TestaTitular.java
@@ -1,8 +1,18 @@
 package entidade;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 public class TestaTitular {
+	
+	DateTime niver1 = new DateTime(1993, 04, 18, 00, 00);
+	DateTime niver2 = new DateTime(1998, 12, 01, 00, 00);
+	DateTime niver3 = new DateTime(1930, 03, 11, 00, 00);
+	DateTime niver4 = new DateTime(1971, 13, 11, 00, 00);
+	
+	Dependente d1 = new Dependente("Ana", niver1);
+	Dependente d2 = new Dependente("Jose", niver2);
+	Dependente d3 = new Dependente("Bruna", niver3);
 	
 	@Test
 	public void TestaBomCidadao() {
